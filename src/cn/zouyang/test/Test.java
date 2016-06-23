dd6b03b164c59e8472f554836e28e08240f8d50e
zouyang <490353104@qq.com>
2016-06-23 15:58:35 +0800

    配置修改

--- src/cn/zouyang/test/Test.java
+++ src/cn/zouyang/test/Test.java
@@ -1,13 +1,24 @@
 package cn.zouyang.test;
 
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.RequestBody;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.ResponseBody;
+
 /**
  * @ClassName: Test
- * @Description: TODO(这里用一句话描述这个类的作用)
+ * @Description: TODO(杩欓噷鐢ㄤ竴鍙ヨ瘽鎻忚堪杩欎釜绫荤殑浣滅敤)
  * @author zouyang
- * @date 2016年6月18日 下午4:06:32
+ * @date 2016骞�鏈�8鏃�涓嬪崍4:06:32
  * 
  */
 
+@Controller
+@RequestMapping("/Test")
 public class Test {
-
+	
+	@RequestMapping(value="/test",produces="application/json;charset=UTF-8")
+	public @ResponseBody String test(){
+		return "测试效果";
+	}
 }
