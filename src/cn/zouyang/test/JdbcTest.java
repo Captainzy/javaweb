07553e6761722090b05f053e5cf76b25e188c499
Captainzy <490353104@qq.com>
2016-07-18 23:16:28 +0800

    jdbc

--- src/cn/zouyang/test/JdbcTest.java
+++ src/cn/zouyang/test/JdbcTest.java
@@ -1,5 +1,13 @@
 package cn.zouyang.test;
 
+import java.sql.Connection;
+import java.sql.DriverManager;
+import java.sql.PreparedStatement;
+import java.sql.ResultSet;
+import java.sql.SQLException;
+import java.sql.Statement;
+
+
 /**
  * @ClassName: JdbcTest
  * @Description: TODO(这里用一句话描述这个类的作用)
@@ -20,6 +28,31 @@ public class JdbcTest {
 	 */
 
 	public static void main(String[] args) {
+		Connection conn = null;
+		PreparedStatement psta = null;
+		Statement sta = null;
+		ResultSet rs = null;
+		try {
+			Class.forName("com.mysql.jdbc.Driver");
+			String userName = "root";
+			String password = "123456";
+			String url = "jdbc:mysql://127.0.0.1:3306/javaweb?user="+userName+"&password="+password+"&useUnicode=true&characterEncoding=UTF8";
+			conn = DriverManager.getConnection(url);
+			
+			String sql = "select * from t_test";
+			psta = conn.prepareStatement(sql);
+			rs = psta.executeQuery();
+			while(rs.next()){
+				String name = rs.getString("name");
+				String pass = rs.getString("password");
+				System.out.println(name + "   " + pass);
+			}
+		} catch (ClassNotFoundException e) {
+			e.printStackTrace();
+		} catch (SQLException e) {
+			// TODO Auto-generated catch block
+			e.printStackTrace();
+		}
 		
 	}
 
