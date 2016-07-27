77cf22bfb68b752c34efce39c46d5bd76ee5f7f9
zouyang <490353104@qq.com>
2016-07-27 18:47:42 +0800

    freemarker

---
+++ src/cn/zouyang/test/FreemarkerTest.java
@@ -0,0 +1,62 @@
+package cn.zouyang.test;
+
+import java.io.BufferedInputStream;
+import java.io.BufferedOutputStream;
+import java.io.BufferedWriter;
+import java.io.File;
+import java.io.FileInputStream;
+import java.io.IOException;
+import java.io.InputStream;
+import java.io.InputStreamReader;
+import java.io.OutputStream;
+import java.io.OutputStreamWriter;
+import java.io.PrintStream;
+import java.io.PrintWriter;
+import java.io.Reader;
+import java.io.Writer;
+import java.util.HashMap;
+import java.util.Map;
+
+import freemarker.template.Configuration;
+import freemarker.template.Template;
+import freemarker.template.TemplateException;
+import freemarker.template.TemplateExceptionHandler;
+
+public class FreemarkerTest {
+
+	public static void main(String[] args) throws IOException, TemplateException {
+		// TODO Auto-generated method stub
+		File dir = new File("C:/Users/zouyang/Desktop/templates");
+		if(!dir.exists()){
+			dir.mkdir();
+		}
+		
+		Configuration cfg = new Configuration(Configuration.VERSION_2_3_25);
+		cfg.setDirectoryForTemplateLoading(dir);
+        cfg.setDefaultEncoding("UTF-8");
+        cfg.setTemplateExceptionHandler(TemplateExceptionHandler.RETHROW_HANDLER);
+        cfg.setLogTemplateExceptions(false);
+        
+        Map<String,Object> root = new HashMap<String,Object>();
+        People peo = new People();
+        peo.setAge("123");
+        peo.setName("test");
+        root.put("people_01", peo);
+        peo = new People();
+        peo.setAge("12");
+        peo.setName("yang");
+        root.put("people_02", peo);
+        
+        File file = null;
+        if(dir.isDirectory()){
+        	file = new File(dir.getPath()+"/testTemplate.ftl");
+        	file.createNewFile();
+        }
+        Reader reader = new InputStreamReader(new FileInputStream(file));
+        
+        Template template =cfg.getTemplate("testTemplate.ftl");
+        Writer writer = new OutputStreamWriter(System.out);
+        template.process(root, writer);
+	}
+
+}
---
+++ src/common/utils/HttpClientUtil.java
@@ -0,0 +1,128 @@
+package common.utils;
+
+import java.io.BufferedInputStream;
+import java.io.File;
+import java.io.FileInputStream;
+import java.io.FileNotFoundException;
+import java.io.IOException;
+import java.io.InputStream;
+import java.io.UnsupportedEncodingException;
+import java.util.ArrayList;
+import java.util.List;
+import java.util.Map;
+
+import org.apache.http.HttpEntity;
+import org.apache.http.HttpResponse;
+import org.apache.http.NameValuePair;
+import org.apache.http.client.ClientProtocolException;
+import org.apache.http.client.HttpClient;
+import org.apache.http.client.entity.UrlEncodedFormEntity;
+import org.apache.http.client.methods.CloseableHttpResponse;
+import org.apache.http.client.methods.HttpGet;
+import org.apache.http.client.methods.HttpPost;
+import org.apache.http.entity.ContentType;
+import org.apache.http.entity.mime.HttpMultipartMode;
+import org.apache.http.entity.mime.MultipartEntity;
+import org.apache.http.entity.mime.MultipartEntityBuilder;
+import org.apache.http.entity.mime.content.FileBody;
+import org.apache.http.entity.mime.content.StringBody;
+import org.apache.http.impl.client.CloseableHttpClient;
+import org.apache.http.impl.client.DefaultHttpClient;
+import org.apache.http.impl.client.HttpClients;
+import org.apache.http.message.BasicNameValuePair;
+import org.apache.http.util.EntityUtils;
+
+public class HttpClientUtil {
+	// post�ύ������Ϊ�ַ�
+	public static String httpPost(String url, Map<String, String> map) {
+		CloseableHttpClient httpClient = HttpClients.createDefault();
+		HttpPost httpPost = new HttpPost(url);
+		CloseableHttpResponse response = null;
+
+		List<NameValuePair> list = new ArrayList<NameValuePair>();
+		for (Map.Entry<String, String> m : map.entrySet()) {
+			NameValuePair nameValuePair = new BasicNameValuePair(m.getKey(), m.getValue());
+			list.add(nameValuePair);
+		}
+
+		HttpEntity postEntity = null;
+		String result = null;
+		try {
+			postEntity = new UrlEncodedFormEntity(list);
+			httpPost.setEntity(postEntity);
+			response = httpClient.execute(httpPost);
+
+			HttpEntity responseEntity = response.getEntity();
+			result = EntityUtils.toString(responseEntity, "UTF-8");
+			// BufferedInputStream bin = new
+			// BufferedInputStream(responseEntity.getContent());
+			// byte[] b = new byte[2048];
+			// bin.read(b);
+			// result = EncodingUtils.getString(b, "UTF-8");
+
+		} catch (UnsupportedEncodingException e) {
+			e.printStackTrace();
+		} catch (ClientProtocolException e) {
+			e.printStackTrace();
+		} catch (IOException e) {
+			e.printStackTrace();
+		}
+
+		return result;
+	}
+
+	// get����
+	public static String httpGet(String url) {
+		CloseableHttpClient httpClient = HttpClients.createDefault();
+		HttpGet httpGet = new HttpGet(url);
+
+		CloseableHttpResponse response = null;
+		HttpEntity responseEntity = null;
+		String result = null;
+		try {
+			response = httpClient.execute(httpGet);
+			responseEntity = response.getEntity();
+			result = EntityUtils.toString(responseEntity, "UTF-8");
+
+		} catch (ClientProtocolException e) {
+			e.printStackTrace();
+		} catch (IOException e) {
+			e.printStackTrace();
+		}
+		return result;
+	}
+
+	// post�ύ������Ϊ��ͬ����
+	public static String postObject(String url, List<File> listFile, Map<String, String> map) throws FileNotFoundException {
+		CloseableHttpClient httpClient = HttpClients.createDefault();
+		HttpPost httpPost = new HttpPost(url);
+		
+		MultipartEntityBuilder entityBuilder = MultipartEntityBuilder.create();
+		entityBuilder.setMode(HttpMultipartMode.BROWSER_COMPATIBLE);
+		for (File f : listFile) {
+			String name = f.getName().substring(f.getName().indexOf("."), f.getName().length());
+			entityBuilder.addBinaryBody(name, f);
+
+		}
+		for (Map.Entry<String, String> e : map.entrySet()) {
+			StringBody stringBody = new StringBody(e.getValue(), ContentType.create("text/plain", "UTF-8"));
+			entityBuilder.addPart(e.getKey(), stringBody);
+		}
+		HttpEntity postEntity = entityBuilder.build();
+		httpPost.setEntity(postEntity);
+		CloseableHttpResponse response = null;
+		HttpEntity responseEntity = null;
+		String result = null;
+		try {
+			response = httpClient.execute(httpPost);
+			responseEntity = response.getEntity();
+			result = EntityUtils.toString(responseEntity);
+		} catch (ClientProtocolException e1) {
+			e1.printStackTrace();
+		} catch (IOException e1) {
+			e1.printStackTrace();
+		}
+		return result;
+	}
+
+}
