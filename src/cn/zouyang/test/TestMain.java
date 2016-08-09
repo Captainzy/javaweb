965e44227aecab8efa8c4cbc1e4f247de3131a98
Captainzy <490353104@qq.com>
2016-08-09 21:55:33 +0800

    wu

--- src/cn/zouyang/test/TestMain.java
+++ src/cn/zouyang/test/TestMain.java
@@ -17,7 +17,6 @@ import java.util.List;
 import java.util.Map;
 import java.util.Vector;
 
-import jdk.nashorn.internal.ir.LiteralNode.ArrayLiteralNode.ArrayUnit;
 import common.utils.SystemUtils;
 
 public class TestMain {
