f3ee2179f50c8334d2e47b5a85c354791d000c72
Captainzy <490353104@qq.com>
2016-08-16 17:29:46 +0800

    servlet

--- src/servletPractice/ServletTest.java
+++ src/servletPractice/ServletTest.java
@@ -1,6 +1,9 @@
 package servletPractice;
 
 import java.io.IOException;
+import java.io.PrintWriter;
+import java.util.Collections;
+import java.util.Enumeration;
 
 import javax.jws.WebParam;
 import javax.servlet.ServletException;
@@ -16,7 +19,21 @@ public class ServletTest extends HttpServlet{
 	@Override
 	protected void doGet(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
 		// TODO Auto-generated method stub
-		System.out.println("servlet 测试");
+		PrintWriter writer = response.getWriter();
+		writer.write("<p>this is a test.</p>");
+		writer.write("<script>alert(123)</script>");
+		Enumeration<String> obj = request.getHeaderNames();
+		while(obj.hasMoreElements()){
+			String headerName = obj.nextElement();
+			System.out.println(headerName + " : " + request.getHeader(headerName));
+		}
+		System.out.println("pathInfo : "+request.getPathInfo());
+		System.out.println("authType : "+request.getAuthType());
+		System.out.println("contentType : "+request.getContentType());
+		System.out.println("contextPath : "+request.getContextPath());
+		System.out.println("pathInfo : "+request.getPathInfo());
+		System.out.println("servletPath : "+request.getServletPath());
+		System.out.println("requestUrl : "+request.getRequestURI());
 	}
 
 }
