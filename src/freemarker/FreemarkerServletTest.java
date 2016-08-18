8e2bc5f31222761bad892ede870843ca8e948001
Captainzy <490353104@qq.com>
2016-08-18 17:27:27 +0800

    freemarker practice

---
+++ src/freemarker/FreemarkerServletTest.java
@@ -0,0 +1,33 @@
+package freemarker;
+
+import java.io.IOException;
+import java.io.Writer;
+import java.util.HashMap;
+import java.util.Map;
+
+import javax.servlet.ServletContext;
+import javax.servlet.ServletException;
+import javax.servlet.annotation.WebServlet;
+import javax.servlet.http.HttpServlet;
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+
+@WebServlet(value="/freemarkerServlet")
+public class FreemarkerServletTest extends HttpServlet {
+
+	@Override
+	protected void doGet(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
+		ServletContext servletContext = request.getServletContext();
+		FreemarkerUtil.initConfiguration(servletContext);
+		Writer writer = response.getWriter();
+		Map<String,Object> map = new HashMap<String,Object>();
+		map.put("author", "zouyang");
+		People p = new People();
+		p.setName("zhang三");
+		p.setAge("13");
+		p.setSex("男");
+		map.put("people", p);
+		FreemarkerUtil.addModelData(map, "freemarker.ftl", writer);
+	}
+	
+}
