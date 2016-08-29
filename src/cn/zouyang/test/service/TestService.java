58838e42e8c44d008b7a70aebe57f8d846ee6e0e
Captainzy <490353104@qq.com>
2016-08-29 23:34:27 +0800

    事务

--- src/cn/zouyang/test/service/TestService.java
+++ src/cn/zouyang/test/service/TestService.java
@@ -1,8 +1,10 @@
 package cn.zouyang.test.service;
 
 import org.springframework.stereotype.Service;
+import org.springframework.transaction.annotation.Transactional;
 
 @Service
+@Transactional
 public class TestService {
 	public void test(){
 		System.out.println("-------------this is a test----------------");
