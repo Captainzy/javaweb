fb6779812b697600e6a89f41abbceb32f0b9d574
Captainzy <490353104@qq.com>
2016-08-30 18:45:32 +0800

    事务

--- src/cn/zouyang/test/Test.java
+++ src/cn/zouyang/test/Test.java
@@ -2,9 +2,12 @@ package cn.zouyang.test;
 
 import javax.servlet.http.Cookie;
 import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
 
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.MediaType;
 import org.springframework.stereotype.Controller;
+import org.springframework.ui.ModelMap;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.ResponseBody;
@@ -26,8 +29,8 @@ public class Test {
 	@Autowired
 	private TestService testService;
 	
-	@RequestMapping(value="/test",produces="application/json;charset=UTF-8")
-	public @ResponseBody String test(){
+	@RequestMapping(value="/test",produces=MediaType.APPLICATION_JSON_UTF8_VALUE)
+	public @ResponseBody String test(HttpServletRequest request,HttpServletResponse response){
 		testService.test();
 		return "测试效果";
 	}
@@ -43,6 +46,12 @@ public class Test {
 	public @ResponseBody String getUserInfoNew(HttpServletRequest request){
 		request.setAttribute("userName", request.getParameter("userName"));
 		request.setAttribute("password", request.getParameter("password"));
+		return "success";
+	}
+	
+	@RequestMapping(value="/{varName}/getPathVariable/{varValue}/{methodName}")
+	public @ResponseBody String getPathVariable(String varName,String varValue,String methodName){
+		System.out.println("varName:"+varName+"\nvarValue:"+varValue+"\nmethodName:"+methodName);	
 		return "success";
 	}
 }
--- src/cn/zouyang/test/service/TestService.java
+++ src/cn/zouyang/test/service/TestService.java
@@ -1,10 +1,11 @@
 package cn.zouyang.test.service;
 
 import org.springframework.stereotype.Service;
+import org.springframework.transaction.annotation.Isolation;
 import org.springframework.transaction.annotation.Transactional;
 
 @Service
-@Transactional
+@Transactional(isolation=Isolation.READ_COMMITTED)
 public class TestService {
 	public void test(){
 		System.out.println("-------------this is a test----------------");
