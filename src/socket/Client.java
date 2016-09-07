78350c81d09242e0ad2730b10df5326fd12d0ab9
Captainzy <490353104@qq.com>
2016-09-07 23:46:41 +0800

    socket

--- src/socket/Client.java
+++ src/socket/Client.java
@@ -1,53 +1,48 @@
 package socket;
 
-import java.io.BufferedReader;
-import java.io.BufferedWriter;
 import java.io.IOException;
 import java.io.InputStreamReader;
 import java.io.OutputStreamWriter;
-import java.io.Reader;
-import java.io.Writer;
 import java.net.Socket;
 import java.net.UnknownHostException;
 
 public class Client {
 	public static void main(String[] args){
 		String host = "127.0.0.1";
-		int port = 8181;
-		Socket client;
-		Writer writer;
-		InputStreamReader reader;
-		
+		int port = 8899;
+		Socket client = null;
+		OutputStreamWriter writer = null;
+		InputStreamReader reader = null;
 		try {
 			client = new Socket(host, port);
+			writer = new OutputStreamWriter(client.getOutputStream(), "UTF-8");
+			reader = new InputStreamReader(client.getInputStream(), "UTF-8");
 			
-			writer = new BufferedWriter(new OutputStreamWriter(client.getOutputStream()));
-			writer.write("hello2 我是客户端");
+			writer.write("客户端：服务端你好");
 			writer.flush();
 			
-			reader = new InputStreamReader(client.getInputStream());
-			char[] cbuf = new char[64];
+			char[] chars = new char[64];
 			StringBuffer sb = new StringBuffer();
 			int len;
-			while((len = reader.read(cbuf))>-1){
-				String str = new String(cbuf);
-				if(str.indexOf("eof")>-1){
+			String str = "";
+			while((len=reader.read(chars))!=-1){
+				str = new String(chars,0,len);
+				if(str.indexOf("eof")!=-1){
 					sb.append(str.substring(0, str.indexOf("eof")));
 					break;
 				}
 				sb.append(str);
-				
 			}
-			System.out.println("这是客户端反馈的信息："+sb);
+			System.out.println("--------------------"+sb.length());
+			System.out.println(sb);
 			
 			writer.close();
 			reader.close();
 			client.close();
+			
 		} catch (UnknownHostException e) {
-			// TODO Auto-generated catch block
 			e.printStackTrace();
 		} catch (IOException e) {
-			// TODO Auto-generated catch block
 			e.printStackTrace();
 		}
 		
