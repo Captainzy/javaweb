009fa41c5d2519f577f464e091976644e7b62c5c
Captainzy <490353104@qq.com>
2016-10-10 15:18:29 +0800

    redis test

--- src/properties/Test.java
+++ src/properties/Test.java
@@ -4,7 +4,7 @@ import java.util.Map;
 
 public class Test {
 	public static void main(String[] args){
-		String filepath = Test.class.getClassLoader().getResource("").getPath()+"ds.properties";
+		String filepath = Test.class.getClassLoader().getResource("").getPath()+"redis.properties";
 		Map<String,String> map = ReadPropertiesUtil.readByProperties(filepath);
 		System.out.println("Properties-->"+map.toString());
 
--- src/redis/JedisTest.java
+++ src/redis/JedisTest.java
@@ -1,9 +1,14 @@
 package redis;
 
 import java.util.List;
+import java.util.Map;
 import java.util.Set;
 
+import properties.ReadPropertiesUtil;
+import properties.Test;
 import redis.clients.jedis.Jedis;
+import redis.clients.jedis.JedisPool;
+import redis.clients.jedis.JedisPoolConfig;
 
 public class JedisTest {
 	public static void main(String[] args){
@@ -29,6 +34,17 @@ public class JedisTest {
 		jedis.lpop("tk");
 		
 		jedis.lset("user", 0, "nothing");
+		String filepath = Test.class.getClassLoader().getResource("").getPath()+"redis.properties";
+		Map<String,String> map = ReadPropertiesUtil.readByProperties(filepath);
+		JedisPoolConfig config = new JedisPoolConfig();
+		config.setMaxIdle(Integer.valueOf(map.get("redis.maxIdle")));
+		config.setMaxWaitMillis(Integer.valueOf(map.get("redis.maxWaitMillis")));
+		config.setMaxTotal(Integer.valueOf(map.get("redis.maxActive")));
+		config.setTestOnBorrow(Boolean.valueOf(map.get("redis.testOnBorrow")));
+		config.setTestOnReturn(Boolean.valueOf(map.get("redis.testOnReturn")));
+		JedisPool jedisPool = new JedisPool(config, "127.0.0.1", 6379);
+		Jedis j = jedisPool.getResource();
+		j.set("test", "this is test of pool");
 		
 	}
 }
