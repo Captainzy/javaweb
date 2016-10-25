2c8caacf78d39878440fc7fe268788015a59a2d2
Captainzy <490353104@qq.com>
2016-10-25 20:35:16 +0800

    nothing

--- src/nio/BaseTestNIO.java
+++ src/nio/BaseTestNIO.java
@@ -7,14 +7,15 @@ import java.nio.channels.FileChannel;
 
 public class BaseTestNIO {
 	public static void main(String[] args) throws Exception{
-		RandomAccessFile file = new RandomAccessFile(new File("C:\\Users\\TZ\\Desktop/nio-data.txt"), "rw");
+		String path = BaseTestNIO.class.getClassLoader().getResource("").getPath()+"/nio/nio-data.txt";
+		RandomAccessFile file = new RandomAccessFile(new File(path), "rw");
 		FileChannel fileChannel = file.getChannel();
 		ByteBuffer buf = ByteBuffer.allocate(1024);
 		int count = fileChannel.read(buf);
 		while(count != -1){
 			buf.flip();
 			byte[] bytes = buf.array();
-			String str = new String(bytes,"gbk");
+			String str = new String(bytes,"UTF-8");
 			System.out.print(str);
 //			while(buf.hasRemaining()){
 //				System.out.print((char)buf.get());
@@ -24,7 +25,6 @@ public class BaseTestNIO {
 		}
 		buf.clear();
 		fileChannel.close();
-		
 		
 	}
 }
