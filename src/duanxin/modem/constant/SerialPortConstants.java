87fab27780de197c544ca715a92febcf77618a6c
Captainzy <490353104@qq.com>
2017-01-22 13:33:26 +0800

    常见的波特率数组

--- src/duanxin/modem/constant/SerialPortConstants.java
+++ src/duanxin/modem/constant/SerialPortConstants.java
@@ -1,7 +1,7 @@
 package duanxin.modem.constant;
 
 public abstract class SerialPortConstants {
-	public static final int[] BAUD_RATE_ARRAY = {57600, 115200};//波特率数组
+	public static final int[] BAUD_RATE_ARRAY = {9600,19200,38400,115200};//常见的波特率数组
 	public static final String MANUFACTURER = "WAVECOM";//短信猫设备提供商
 	public static final int CARDCOUNT = 4;//短信猫里插的能工作的电话卡的数量
 	public static final boolean DEBUG = false;//是否调试模式
