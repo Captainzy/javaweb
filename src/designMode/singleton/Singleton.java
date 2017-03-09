f91c23f81005e0ad08ac94ddeb92b779ed745465
Captainzy <490353104@qq.com>
2017-03-09 14:53:33 +0800

    枚举实现单例模式

--- src/designMode/singleton/Singleton.java
+++ src/designMode/singleton/Singleton.java
@@ -1,36 +1,13 @@
-package designMode;
+package designMode.singleton;
 
 /**
  * 单例模式的各种写法，由于都是内部类所以都采用static修饰class关键字，但是在实际开发中不是内部类则不用static修饰class
  */
 public class Singleton {
-
-	/**
-	 * @author zouyang
-	 * @time 2017年2月7日 下午4:51:35
-	 * @description 枚举写法，最为安全的实现单例的方法是通过内部静态enum的方法来实现，
-	 *              因为JVM会保证enum不能被反射并且构造器方法只执行一次。 因此，枚举写法是单例模式推荐写法。 
-	 *使用：直接 Singleton_05.SINGLETON.getSingletonInstance();
-	 */
-	public static enum Singleton_05 {
-		/**
-		 * 通过enum关键字来实现枚举，在枚举中需要注意的有：
-		 *  1. 枚举中的属性必须放在最前面，一般使用大写字母表示 
-		 *  2. 枚举中可以和java类一样定义方法 
-		 *  3. 枚举中的构造方法必须是私有的
-		 */
-		SINGLETON;
-		private Singleton singleton;
-
-		private Singleton_05() {
-			singleton = new Singleton();
-		}
-
-		public Singleton getSingletonInstance() {
-			return singleton;
-		}
-	}
-
+	
+	//构造函数用protected修饰可以保证其他包不能通过new关键字构造实例对象，这样就方便利用枚举的方式实现单例模式
+	protected Singleton(){}
+	
 	/**
 	 * @author zouyang
 	 * @time 2017年2月7日 下午4:00:24
@@ -38,9 +15,8 @@ public class Singleton {
 	 */
 	public static class Singleton_01 {
 		private static Singleton_01 singleton;
-
-		private Singleton_01() {
-		}
+		
+		private Singleton_01() {}
 
 		public Singleton_01 getInstance() {
 			if (singleton == null) {
